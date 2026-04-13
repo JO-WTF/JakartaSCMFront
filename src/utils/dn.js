2d57500fc303d3e2c6fb12c655845c9132b6eddc
Jo WTF <zhongzhyu@gmail.com>
2026-04-13 20:39:20 +0800

    修改DN校验规则为2-5位字母+10-15位数字

--- src/utils/dn.js
+++ src/utils/dn.js
@@ -1,6 +1,6 @@
 const DN_ZERO_WIDTH_RE = /[\u200B\u200C\u200D\u2060\uFEFF]/g;
-export const DN_VALID_RE = /^[A-Z]{3,5}\d{9,15}$/;
-const DN_VALID_WITH_SUFFIX_RE = /^([A-Z]{3,5}\d{9,15})B\d{2,3}$/;
+export const DN_VALID_RE = /^[A-Z]{2,5}\d{10,15}$/;
+const DN_VALID_WITH_SUFFIX_RE = /^([A-Z]{2,5}\d{10,15})B\d{2,3}$/;
 
 export function normalizeDnSoft(raw) {
   return (raw || '').replace(DN_ZERO_WIDTH_RE, '').toUpperCase();
